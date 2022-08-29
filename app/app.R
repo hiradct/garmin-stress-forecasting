90acbdf975c0ed1c2ff8dda06c2f26512a31b6fa
Oleh <olegante10@gmail.com>
2022-08-29 19:25:09 -0400

    added shiny app setup

--- app/app.R
+++ app/app.R
@@ -1,1 +1,14 @@
-# this is edit
+library(shiny)
+
+# Define UI for the app
+ui <- fluidPage(
+  # App title
+  titlePanel("Team Chernoff")
+)
+
+# Define server logic for the app
+server <- function(input, output) {
+}
+
+# Create Shiny app
+shinyApp(ui = ui, server = server)
