52941cc9bde1c76fed811081bcdf1191d943d6f8
Oleh <olegante10@gmail.com>
2022-09-07 20:32:27 -0400

    added multiple tabs to main panel

--- app/app.R
+++ app/app.R
@@ -3,17 +3,36 @@ library(shiny)
 # Define UI for the app
 ui <- fluidPage(
   # App title
-  navbarPage("Team Chernoff",
-             tabPanel("Charts"),
-             tabPanel("Model"),
-             tabPanel("Summary"),
-             tabPanel("Other")
+  titlePanel("Forecasting Garmin by Team Chernoff 😎"),
+  br(),
+  sidebarLayout(
+    sidebarPanel(
+    selectInput("metric", "Metrics:",
+                  list("Heart Rate" = "heart rate", 
+                       "Stress" = "stress level",
+                       "Body Battery" = "body battery")),
+      numericInput("ahead", "Hours (STC) to forecast:", 1),
+      
+      submitButton("Update view")
+    ),
+    mainPanel(
+      h2(textOutput("panelTitle")),
+      tabsetPanel(
+        tabPanel("Actual plot", plotOutput("actualPlot")), 
+        tabPanel("Forecasted plot", plotOutput("forecastPlot")), 
+        tabPanel("Overlay plot", plotOutput("overlayPlot")), 
+        tabPanel("Model Summary", dataTableOutput("summary"))
+    ))
   )
 )
 
 # Define server logic for the app
 server <- function(input, output) {
+  output$panelTitle <- renderText({
+    paste("Plotting ", input$metric)
+  })
 }
+
 
 # Create Shiny app
 shinyApp(ui = ui, server = server)
