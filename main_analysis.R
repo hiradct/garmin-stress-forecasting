7bc3a0f2c462c4414df4f2895b5b717cb4e0602e
hiradct <hirad.cheraghi@gmail.com>
2022-09-15 19:01:20 -0400

    Created SARIMA models and evals for varying hist. size

--- main_analysis.R
+++ main_analysis.R
@@ -17,14 +17,14 @@ library(ggcorrplot)
 library(mosaic)
 library(pROC)
 library(Hmisc)
-#install.packages('forecast')
+install.packages('Metrics')
 library(forecast)
 library(naniar)
 library(Metrics)
 
 
-#### STEP_2 - Read the data ####
-garmin_raw_data <- read.csv('/Users/hiradch/Desktop/garmin-predictive-modeling/garmin_final.csv')
+#### STEP_2 - Read the data - READ ONLY LATEST FINAL CSV ####
+garmin_raw_data <- read.csv('/Users/hiradch/Desktop/garmin-predictive-modeling/garmin_upd.csv')
 
 #select only times, HR and stress
 garmin_raw_df <- subset(garmin_raw_data, select = c(1,5,15))
@@ -41,12 +41,23 @@ garmin_raw_df$month <- as.integer(strftime(garmin_raw_df$date, format = "%m"))
 #garmin_raw_df$day <- wday(garmin_raw_df$date, week_start=1)
 garmin_raw_df$hour <- as.integer(format(as.POSIXct(garmin_raw_df$timestamp),"%H"))
 garmin_raw_df$min <- as.integer(format(as.POSIXct(garmin_raw_df$timestamp),"%M"))
+
 #reorder columns
 garmin_series <- select(garmin_raw_df,1,4,6,5,7:8,2,3)
 
 
 ## REMOVE SOME WEEKS 
-#--->1) WEEK #22 - first week reading on device is not as accurate
+#--->1) WEEK #19-22 - first week reading on device is not as accurate
+
+#reorder columns
+garmin_series <- select(garmin_raw_df,1,4,6,5,7:8,2,3)
+
+## REMOVE SOME WEEKS 
+#--->1) WEEK #19-22 - first week reading on device is not as accurate
+#---> Remaining Weeks --.> 23-36 --> 14 weeks
+# Value         23    24    25    26    27    28    29    30    31    32    33    34    35    36
+# Frequency   5040  5040  5040  5040  5040  5040  5040  5040  5040  5040  5040  5040  5040  4321
+# Proportion 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.072 0.062
 
 library(naniar)
 library("imputeTS")
@@ -56,11 +67,13 @@ library("imputeTS")
 # ggplot_na_intervals(garmin_series$heartRate, interval_size = 720, measure = "count",color_missing = "gold3")
 # ggplot_na_distribution(garmin_series$heartRate, x_axis_labels = garmin_series$date)
 # # ggplot_na_intervals(garmin_series_f$heartRate, interval_size = 720,color_missing = "gold3")
-# ggplot_na_distribution(garmin_series$stressLevel, x_axis_labels = garmin_series$date)
-# ggplot_na_intervals(garmin_series$stressLevel, interval_size = 720,color_missing = "gold3")
+ggplot_na_distribution(garmin_series$stressLevel, x_axis_labels = garmin_series$date)
+ggplot_na_intervals(garmin_series$stressLevel, interval_size = 720,color_missing = "gold3")
 
 garmin_df <-garmin_series[!(garmin_series$week %in% c(19,20,21,22)),]
+ggplot_na_distribution(garmin_df$stressLevel, x_axis_labels = garmin_df$timestamp)
 
+plot.ts(as.ts(garmin_df$stressLevel))
 
 #REMOVED 5,000 datapoints, from 13 to 10 weeks
 
@@ -127,7 +140,7 @@ ggplot_na_distribution(garmin_df_inter$heartRate)
 str(garmin_df_inter)
 
 
-#### STEP_6 - Create TS Objects & Resample to Hourly ####
+#### STEP_6 - Resample to Hourly ####
 
 #-Create separate data frames
 garmin_df_stress = garmin_df_inter[,c(1,8)]
@@ -148,55 +161,56 @@ garmin_df_hr.hourly$timestamp2 <- as.POSIXct(garmin_df_hr.hourly$timestamp2, tz 
 garmin_df_stress.hourly$stressLevel <- as.integer(round(garmin_df_stress.hourly$stressLevel, 0))
 garmin_df_hr.hourly$heartRate <- as.integer(round(garmin_df_hr.hourly$heartRate, 0))
 
-#-Create time series objects using XTS
-#https://www.datacamp.com/cheat-sheet/xts-cheat-sheet-time-series-in-r
+
+
+#### STEP_6 - Split data into diff Hist Vectors + Predition period + DEMO 2-day forecast set####
+
+
+# Datasets --> garmin_df_stress.hourly & garmin_df_hr.hourly
+#---lowest : 2022-06-06 00:00:00
+#---highest: 2022-09-10 20:00:00
+
+#-Create time series objects using XTS and ts
 library(xts)
 stress.xts <- as.xts(garmin_df_stress.hourly[,"stressLevel"],garmin_df_stress.hourly$timestamp2,dateFormat="POSIXct")
 plot.xts(stress.xts)
-
-heart.rate.xts = as.xts(garmin_df_hr.hourly[,"heartRate"],garmin_df_hr.hourly$timestamp2,dateFormat="POSIXct")
-plot.xts(heart.rate.xts)
-
-#--> stress.xts
-#---> heart.rate.xts
-
-#Create TS objects from the XTS objects as well
 library(tsbox)
 stress.ts <-ts_ts(stress.xts)
-heart.rate.ts <-ts_ts(heart.rate.xts)
 
-plot.ts(stress.ts)
-plot.ts(heart.rate.ts)
+# 4 day prediction from 7 day history
 
+#DEMO Forecast Set --> ["2022-09-09/2022-09-10"] (2 days)
+#TEST --> ["2022-09-05/022-09-08"] (4 days)
 
-#### STEP_6 - SPLIT into HIST, SHORT-HIST & DEMO 2-day TEST####
+# demo.test.xts <- stress.xts["2022-09-09/"]
+# demo.hist.xts = stress.xts["2022-09-01/2022-09-08"]
+TEST.xts = stress.xts["2022-09-05/2022-09-08"] 
 
-# Datasets --> garmin_df_stress.hourly & garmin_df_hr.hourly
+#PAST_1 --> ["2022-08-29/2022-09-04"] (1 week) (7 days) (Mon-Sunday)
+#PAST_2 --> ["2022-08-22/2022-09-04"] (2 weeks) (14 days)
+#PAST_4 --> ["2022-08-08/2022-09-04"] (4 weeks) (28 days) 
+#PAST_8 --> ["2022-07-11/2022-09-04"] (8 weeks) (56 days) 
+#PAST_12 --> ["2022-06-13/2022-09-08"] (12 weeks) (84 days) 
 
-first(stress.xts) # 2022-06-06
-last(stress.xts) # 2022-09-11
-#remove the last 2 days
-stress.xts <- stress.xts["/2022-09-10"]
-last(stress.xts) # 2022-09-10
-
-
-#train.xts = stress.xts["2022-06-08/2022-09-08"] #train set of 3 months
-#train.xts = stress.xts["2022-07-08/2022-09-08"] #train set of 2 months
-train.xts = stress.xts["2022-08-08/2022-09-08"] #train set of 1 months
-
-last(train.xts)
-
-demo.test.xts = stress.xts["2022-09-09/"] # test set of 2 days
-first(demo.test.xts)
-
-demo.hist.xts = stress.xts["2022-09-01/2022-09-08"]
-first(demo.hist.xts)
-last(demo.hist.xts)
+PAST_1.xts = stress.xts["2022-08-29/2022-09-04"]
+PAST_2.xts = stress.xts["2022-08-22/2022-09-04"] 
+PAST_4.xts = stress.xts["2022-08-08/2022-09-04"] 
+PAST_8.xts = stress.xts["2022-07-11/2022-09-04"]
+PAST_12.xts = stress.xts["2022-06-13/2022-09-08"]
 
 
-train.ts <-ts_ts(train.xts)
-demo.test.ts <-ts_ts(demo.test.xts)
-demo.hist.ts <-ts_ts(demo.hist.xts)
+
+
+# #train.xts = stress.xts["2022-06-08/2022-09-08"] #train set of 3 months
+# #train.xts = stress.xts["2022-07-08/2022-09-08"] #train set of 2 months
+# train.xts = stress.xts["2022-08-08/2022-09-04"] #train set of 1 months
+# 
+# train.ts <-ts_ts(train.xts)
+# demo.test.ts <-ts_ts(demo.test.xts)
+# demo.hist.ts <-ts_ts(demo.hist.xts)
+# 
+# plot.ts(stress.ts)
+
 
 
 
@@ -206,17 +220,22 @@ demo.hist.ts <-ts_ts(demo.hist.xts)
 library(forecast)
 library(fpp)
 
-plot.xts(train.xts)
-plot.ts(train.ts)
+plot.xts(PAST_4.xts)
+PAST_4.ts <- ts_ts(PAST_4.xts)
+plot.ts(PAST_4.ts)
 
-data <- ts(train.ts, frequency = 24) 
-fit <- stl(data,"periodic") # estimate the daily cyclical component
-autoplot(fit)
 
-#deaseason the data ro remove the pattern that occur every day
-train.ts_deSeason = data - seasonal(fit)
-plot.ts(train.ts)
-plot.ts(train.ts_deSeason)
+#Decompose on daily
+data_d <- ts(PAST_4.ts, frequency = 24) 
+fit_d <- stl(data_d,"periodic") # estimate the daily cyclical component
+autoplot(fit_d)
+
+
+#Decompose on weekly
+data_w <- ts(PAST_4.ts, frequency = 168) 
+fit_w <- stl(data_w,"periodic") # estimate the daily cyclical component
+autoplot(fit_w)
+
 
 ## -----Check for hetero Does the variance increases with mean (heteroscedasticity)?-----
 #--> Statistical Testing -->  NO HETERO
@@ -281,29 +300,226 @@ plot.ts(train.ts_deSeason_diff) #TAKE OUT TRENDING
 
 
 
-#-------------Modelling via Automatic ARIMA --------------------
 
-model.auto <- auto.arima(train.xts, seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE, max.p = 10, max.q = 10) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
-#---auto AIMA IS STUPID
+#-------------Model Build with varying training history period via Automatic ARIMA --------------------
 
-# It suggests a ARIMA(5,0, 0) model with non-zero mean, 
-checkresiduals(model.auto)  # Check the quality of fit. Residuals should: 
-# The p-values for the Ljung–Box statistics are small, 
+### One Week Training Window, Trained Model --> (1,0,1)(0,1,1) ####
+
+#Obtain non-seasonal params from Auto ARIMA --> RESULT: (1,0,1)
+
+model_1b <- auto.arima(PAST_1.xts, seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE, max.p = 10, max.q = 10) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
+checkresiduals(model_1) # The p-values for the Ljung–Box statistics are LARGE,--> ARIMA is sufficient 
 #....indicating there is some pattern in the residuals. There is still information to be extracted from the data.
 # so this means that the daily cyclical pattern requires us to use SARIMA instead of ARIMA
 
-fit_season <- Arima(train.xts, order=c(4,0,1), seasonal=list(order=c(0,1,1),period=24))
-fit_season
-# Plot the forecasting in the original scale
-fc1<-forecast(fit_season,48)
-autoplot(fc1)
 
-summary(fit_season)
+#Manually incorporate order=c(0,1,1) for seasonal component
+model_1_SARIMA <- Arima(PAST_1.xts, order=c(1,0,1), seasonal=list(order=c(0,1,1),period=24))
+summary(model_1_SARIMA)
+checkresiduals(model_1_SARIMA)
+#sarima is a better fit
+
+model_1
+model_1_SARIMA
+
+####Past 1 Test --> fcp1_RMSE = 18.43 #####
+
+forecasted_past1 = sarima.for(PAST_1.xts,96,1,0,1,0,1,1,24)
+
+forecasted_past1_df <- as.data.frame(forecasted_past1) #input forecasts to dataframe
+TEST1_df <- as.data.frame(TEST.xts) #input actuals to dataframe
+names(forecasted_past1_df)[1] <- 'Predicted_Stress'
+names(TEST1_df)[1] <- 'Actual_Stress'
+TEST1_PAST_1_comb<- cbind(forecasted_past1_df,TEST1_df)
+
+fcp1_MSE <- mean((TEST1_PAST_1_comb$Predicted_Stress-TEST1_PAST_1_comb$Actual_Stress)^2) #calculate and display MSE in the testing set
+#MSE, is calculated as the average of the squared forecast error values. Squaring the forecast error values forces them to be positive; 
+#it also has the effect of putting more weight on large errors.
+fcp1_RMSE <- rmse(TEST1_PAST_1_comb$Actual_Stress, TEST1_PAST_1_comb$Predicted_Stress) #calculate and display RMSE 
+# MSE can be transformed back into the original units of the predictions by taking the square root of the mean squared error score
+
+TEST1_PAST_1_comb.xts <- as.xts(TEST1_PAST_1_comb)
+
+myplot_test1 <- plot.xts(TEST1_PAST_1_comb.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
+lines(TEST1_PAST_1_comb.xts$Predicted_Stress, col = "Red", lwd = 3)
+
+
+
+### Two Week Training Window, Trained Model --> (4,0,1)(0,1,1) ####
+
+model_2 <- auto.arima(PAST_2.xts,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE, max.p = 10, max.q = 10) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
+
+checkresiduals(model_2) 
+summary(model_2)
+
+model_2_SARIMA <- Arima(PAST_2.xts, order=c(4,0,1), seasonal=list(order=c(0,1,1),period=24))
+#model_2_SARIMA_2 <- sarima(PAST_2.xts,4,0,1,0,1,1,24) #using SARIMA function
+summary(model_2_SARIMA)
+checkresiduals(model_2_SARIMA)
+
+
+model_2
+model_3_SARIMA
+
+####Past 2 Test --> fcp2_RMSE = 15.80776 #####
+
+forecasted_past2 = sarima.for(PAST_2.xts,96,4,0,1,0,1,1,24)
+
+forecasted_past2_df <- as.data.frame(forecasted_past2) #input forecasts to dataframe
+#TEST1_df <- as.data.frame(TEST.xts) #input actuals to dataframe
+names(forecasted_past2_df)[1] <- 'Predicted_Stress'
+#names(TEST1_df)[1] <- 'Actual_Stress'
+TEST1_PAST_2_comb<- cbind(forecasted_past2_df,TEST1_df)
+
+fcp2_MSE <- mean((TEST1_PAST_2_comb$Predicted_Stress-TEST1_PAST_2_comb$Actual_Stress)^2) #calculate and display MSE in the testing set
+fcp2_RMSE <- rmse(TEST1_PAST_2_comb$Actual_Stress, TEST1_PAST_2_comb$Predicted_Stress) #calculate and display RMSE 
+
+TEST1_PAST_2_comb.xts <- as.xts(TEST1_PAST_2_comb)
+
+myplot_test1 <- plot.xts(TEST1_PAST_2_comb.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
+lines(TEST1_PAST_2_comb.xts$Predicted_Stress, col = "Red", lwd = 3)
+
+fcp2_RMSE
+
+
+### Four Week Training Window, Trained Model --> (4,0,1)(0,1,1) ####
+
+model_3 <- auto.arima(PAST_4.xts, max.order = 20,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
+# model_3b <- auto.arima(PAST_4.xts,D=1,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE)
+# 
+# Lambda <- BoxCox.lambda(ts_ts(PAST_4.xts))
+# model_3c <- auto.arima(PAST_4.xts,lambda = Lambda,D=1,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE, max.p = 10, max.q = 10)
+
+checkresiduals(model_3) 
+summary(model_3)
+
+model_3_SARIMA <- Arima(PAST_4.xts, order=c(4,0,1), seasonal=list(order=c(0,1,1),period=24))
+#model_3_SARIMA_2 <- sarima(PAST_2.xts,4,0,1,0,1,1,24) #using SARIMA function
+summary(model_3_SARIMA)
+checkresiduals(model_3_SARIMA)
+
+
+model_3
+model_3_SARIMA
+
+####Past 4 Test --> fcp4_RMSE = 15.80907 #####
+
+forecasted_past4 = sarima.for(PAST_4.xts,96,4,0,1,0,1,1,24)
+
+forecasted_past4_df <- as.data.frame(forecasted_past4) #input forecasts to dataframe
+#TEST1_df <- as.data.frame(TEST.xts) #input actuals to dataframe
+names(forecasted_past4_df)[1] <- 'Predicted_Stress'
+#names(TEST1_df)[1] <- 'Actual_Stress'
+TEST1_PAST_4_comb<- cbind(forecasted_past4_df,TEST1_df)
+
+fcp4_MSE <- mean((TEST1_PAST_4_comb$Predicted_Stress-TEST1_PAST_4_comb$Actual_Stress)^2) #calculate and display MSE in the testing set
+fcp4_RMSE <- rmse(TEST1_PAST_4_comb$Actual_Stress, TEST1_PAST_4_comb$Predicted_Stress) #calculate and display RMSE 
+
+TEST1_PAST_4_comb.xts <- as.xts(TEST1_PAST_4_comb)
+
+myplot_test1 <- plot.xts(TEST1_PAST_4_comb.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
+lines(TEST1_PAST_4_comb.xts$Predicted_Stress, col = "Red", lwd = 3)
+
+fcp4_RMSE
+
+
+
+
+
+
+### Eight Week Training Window, Trained Model --> (0,1,3)(0,1,1)####
+
+#Lambda <- BoxCox.lambda(PAST_8.xts)
+#D=1,lambda = Lambda
+model_4 <- auto.arima(PAST_8.xts,max.order = 20,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
+measures(ts_ts(PAST_8.xts))
+
+checkresiduals(model_4) 
+summary(model_4)
+
+model_4_SARIMA <- Arima(PAST_8.xts, order=c(0,1,3), seasonal=list(order=c(0,1,1),period=24))
+#model_3_SARIMA_2 <- sarima(PAST_2.xts,4,0,1,0,1,1,24) #using SARIMA function
+summary(model_4_SARIMA)
+checkresiduals(model_4_SARIMA)
+
+
+model_4
+model_4_SARIMA
+
+####Past 8 Test --> fcp3_RMSE = 15.77468 #####
+
+forecasted_past8 = sarima.for(PAST_8.xts,96,1,0,3,0,1,1,24)
+
+forecasted_past8_df <- as.data.frame(forecasted_past8) #input forecasts to dataframe
+#TEST1_df <- as.data.frame(TEST.xts) #input actuals to dataframe
+names(forecasted_past8_df)[1] <- 'Predicted_Stress'
+#names(TEST1_df)[1] <- 'Actual_Stress'
+TEST1_PAST_8_comb<- cbind(forecasted_past8_df,TEST1_df)
+
+fcp8_MSE <- mean((TEST1_PAST_8_comb$Predicted_Stress-TEST1_PAST_8_comb$Actual_Stress)^2) #calculate and display MSE in the testing set
+fcp8_RMSE <- rmse(TEST1_PAST_8_comb$Actual_Stress, TEST1_PAST_8_comb$Predicted_Stress) #calculate and display RMSE 
+
+TEST1_PAST_8_comb.xts <- as.xts(TEST1_PAST_8_comb)
+
+myplot_test1 <- plot.xts(TEST1_PAST_8_comb.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
+lines(TEST1_PAST_8_comb.xts$Predicted_Stress, col = "Red", lwd = 3)
+
+fcp8_RMSE
+
+
+### Twelve  Week Training Window, Trained Model --> (2,0,4)(0,1,1) ####
+
+#Lambda <- BoxCox.lambda(PAST_8.xts)
+#D=1,lambda = Lambda
+model_5 <- auto.arima(PAST_12.xts,max.order = 20,seasonal= TRUE,approximation = FALSE,trace=TRUE, stepwise = FALSE) #Fit using the Hyndman-Khandakar algorithm (Hyndman & Khandakar, 2008)
+measures(ts_ts(PAST_12.xts))
+
+checkresiduals(model_5) 
+summary(model_5)
+
+model_4_SARIMA <- Arima(PAST_8.xts, order=c(2,0,4), seasonal=list(order=c(0,1,1),period=24))
+#model_3_SARIMA_2 <- sarima(PAST_2.xts,4,0,1,0,1,1,24) #using SARIMA function
+summary(model_4_SARIMA)
+checkresiduals(model_4_SARIMA)
+
+####Past 12 Test --> fcp3_RMSE =  15.45822 #####
+
+forecasted_past12 = sarima.for(PAST_12.xts,96,2,0,4,0,1,1,24)
+
+forecasted_past12_df <- as.data.frame(forecasted_past12) #input forecasts to dataframe
+#TEST1_df <- as.data.frame(TEST.xts) #input actuals to dataframe
+names(forecasted_past12_df)[1] <- 'Predicted_Stress'
+#names(TEST1_df)[1] <- 'Actual_Stress'
+TEST1_PAST_12_comb<- cbind(forecasted_past12_df,TEST1_df)
+
+fcp12_MSE <- mean((TEST1_PAST_12_comb$Predicted_Stress-TEST1_PAST_12_comb$Actual_Stress)^2) #calculate and display MSE in the testing set
+fcp12_RMSE <- rmse(TEST1_PAST_12_comb$Actual_Stress, TEST1_PAST_12_comb$Predicted_Stress) #calculate and display RMSE 
+
+TEST1_PAST_12_comb.xts <- as.xts(TEST1_PAST_12_comb)
+
+myplot_test1 <- plot.xts(TEST1_PAST_12_comb.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
+lines(TEST1_PAST_12_comb.xts$Predicted_Stress, col = "Red", lwd = 3)
+geom_ribbon(aes(ymin=TEST1_PAST_12_comb.xts$Predicted_Stress-TEST1_PAST_12_comb.xts$se,
+                ymax=TEST1_PAST_12_comb.xts$Predicted_Stress+TEST1_PAST_12_comb.xts$se),fill="grey70") + geom_line()
+
+fcp12_RMSE
+
+
+
+
+
+
+
+
+#### Line Prediction vs Actual Plots of all 5 models ####
+
+
+
 
 ##TRY the same model using SARIMA function
 #fit_model = sarima(train.xts,4,0,1,0,1,1,24)
 
-##-----------FINAL MODEL PARAMETERS-------##
+##-----------OLD FINAL MODEL PARAMETERS-------##
 p <- 4
 d <- 0
 q <- 1
@@ -351,7 +567,7 @@ write.csv(demo_ouput_df,'F:\\Smith MMA\\MMA867 Predictive Modelling\\Term Projec
 
 demo_ouput_df.xts <- as.xts(demo_ouput_df)
 
-myplot <- plot.xts(demo_ouput_df.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3,, grid.col = NA, col = "Blue")
+myplot <- plot.xts(demo_ouput_df.xts$Actual_Stress, main = "Actual vs Forecasted Stress", lwd = 3, grid.col = NA, col = "Blue")
 lines(demo_ouput_df.xts$Predict_Stress, col = "Green", lwd = 3)
 
 fc_MSE <- mean((demo_ouput_df$Predict_Stress-demo_ouput_df$Actual_Stress)^2) #calculate and display MSE in the testing set
@@ -363,8 +579,10 @@ fc_MAPE <-mean(abs(demo_ouput_df$Predict_Stress-demo_ouput_df$Actual_Stress)/dem
 
 
 
-####----------->>>>>>Heart Rate Prediction<<<<<------------####
+####----------->>>>>>Seasonal Naive Modeling - BENCHMARK<<<<<------------####
 
+
+sales.fc <- snaive(sales.ts, h=4)
 
 #### We pick up from STEP_6 - SPLIT into HIST, SHORT-HIST & DEMO 2-day TEST####
 
